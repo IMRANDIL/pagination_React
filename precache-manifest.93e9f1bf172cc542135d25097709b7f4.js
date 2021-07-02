01fa8f1250240bdcc0ae4cfed606b74f02f28f11
IMRANDIL <aliimranadil2@gmail.com>
2021-07-02 18:15:59 +0530

    Updates

---
+++ precache-manifest.93e9f1bf172cc542135d25097709b7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ae0ca4bbdab564eb4a1eb605095001",
+    "url": "/pagination_React/index.html"
+  },
+  {
+    "revision": "c5fa852ccfe00d559a5a",
+    "url": "/pagination_React/static/css/main.980c564c.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/pagination_React/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pagination_React/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fa852ccfe00d559a5a",
+    "url": "/pagination_React/static/js/main.87e2d453.chunk.js"
+  },
+  {
+    "revision": "6ffdb231bda9d040860a",
+    "url": "/pagination_React/static/js/runtime-main.0e258a78.js"
+  }
+]);
